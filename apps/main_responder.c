f751e0dd21814c02a683ac5e0d7d2bfb5c421f5d
rgunjin <boyfromburn@gmail.com>
2026-03-26 12:36:32 +0100

    removed printf debug

--- apps/main_responder.c
+++ apps/main_responder.c
@@ -108,25 +108,10 @@ int main(void) {
     while (1) {
         // 1. Enable RX and wait for poll
         dw1000_rx_enable();
-        SEGGER_RTT_printf(0, "[RX] listening...\n");
-
-        uint32_t sys_ctrl = dw1000_read32(DW_REG_SYS_CFG);
-        uint32_t sys_status = dw1000_read_sys_status();
-        SEGGER_RTT_printf(0, "[RX] SYS_CTRL=0x%08X SYS_STATUS=0x%08X\n", sys_ctrl, sys_status);
 
         uint32_t status;
-        uint32_t timeout = 0;
         while (!((status = dw1000_read_sys_status()) &
-                    (SYS_STATUS_RXFCG | SYS_STATUS_ALL_RX_ERR))) {
-            timeout++;
-            if (timeout > 100000) {
-                SEGGER_RTT_printf(0, "[RX] timeout, status=0x%08X\n", status);
-                break;
-            }
-        }
-
-        
-        SEGGER_RTT_printf(0, "[RX] status=0x%08X\n", status);
+                    (SYS_STATUS_RXFCG | SYS_STATUS_ALL_RX_ERR))) {}
 
         if (status & SYS_STATUS_RXFCG) {
             // 2. Read recieved frame
